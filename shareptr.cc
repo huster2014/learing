17c8be5c6e4fc3a382049959b706448186464e9c
tanghao <1027411773@qq.com>
2015-03-22 14:55:00 +0800

    Delete shareptr.cc

--- shareptr.cc
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main()
-{
-	std::cout << "this is my first time to use github" << std::endl;
-	for (int i=0; i<1024; i++){
-		std::cout << "github is very easy to use!!" <<std::endl;
-	}
-}
