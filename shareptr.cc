030c7be8b59511869cb03cfefd80a78f47a68daf
tanghao <1027411773@qq.com>
2015-03-22 14:45:18 +0800

    change the content of main()

--- shareptr.cc
+++ shareptr.cc
@@ -1,23 +1,6 @@
-#include <boost/shared_ptr.hpp> 
-#include <memory>
 #include <iostream>
-class RemoveOnDelete {
-  public:
-    RemoveOnDelete(){};
-    void operator() (int *x) {
-    	std::cout << "delete " << *x << std::endl;
-    }
-}; // 起到delete的作用
 
-typedef boost::shared_ptr<int> vint;
-
-vint create_inter(int x)
+int main()
 {
-	return vint(new int(x), RemoveOnDelete());
-}
-int main()  
-{      
-	for (int t = 0; t<10; t++) {
-		vint v = create_inter(t);
-	}
+	std::cout << "this is my first time to use github" << std::endl;
 }
